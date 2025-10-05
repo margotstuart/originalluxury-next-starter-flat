da7fa79b1b0b7067baf6479813441860aea96997
margotstuart <75415695+margotstuart@users.noreply.github.com>
2025-10-05 14:05:43 +0400

    Delete tailwind.config.ts

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
-export default config;
