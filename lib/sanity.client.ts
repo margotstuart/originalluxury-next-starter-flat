f38d73f57474ce3ce41a7de9cd45f60a682e10a9
margotstuart <75415695+margotstuart@users.noreply.github.com>
2025-10-05 13:45:17 +0400

    Create sanity.client.ts

---
+++ lib/sanity.client.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@sanity/client';
+
+export const apiVersion = process.env.SANITY_API_VERSION || '2023-10-10';
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const token = process.env.SANITY_READ_TOKEN;
+const useCdn = !token; // if token present (private dataset), bypass CDN
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn,
+  token,
+  perspective: 'published',
+});
